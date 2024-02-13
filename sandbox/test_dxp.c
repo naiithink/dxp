ac1dedb6246f595a8aac0e54037f0064fb00d82f
Potsawat Thinkanwatthana <potsawat.think@gmail.com>
2024-02-14 05:46:20 +0700

    Checkout lib source from 'naiithink/pastree'
    Checked out library source from <https://github.com/naiithink/pastree.git>
    at da9f235.
    
    Full ref: <https://github.com/naiithink/pastree/tree/da9f2353c574fcb8eecae7d03c7d492fe2166f10/lib/libdxp>

---
+++ sandbox/test_dxp.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/dxp/dxp.h"
+
+int main(int argc, char **argv) { return EXIT_SUCCESS; }
---
+++ sandbox/test_dxp_method.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/dxp/dxp_method.h"
+
+int main(int argc, char **argv) { return EXIT_SUCCESS; }
